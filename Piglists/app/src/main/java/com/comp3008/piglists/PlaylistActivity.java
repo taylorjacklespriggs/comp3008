3ec23248b8eaa6a23e9132ce854016a3d5654807
Charron, Christopher <charch84@gmail.com>
2016-03-29 12:33:09 -0400

    Commiting the initial project. Currently there is only a blank activity.
    Let's get this going!

---
+++ Piglists/app/src/main/java/com/comp3008/piglists/PlaylistActivity.java
@@ -0,0 +1,13 @@
+package com.comp3008.piglists;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+    }
+}
